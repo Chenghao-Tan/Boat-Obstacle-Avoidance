ab9fcae2bcd204b286d5c57ae847ae3b95907510
Chenghao Tan <tch888free@gmail.com>
2022-08-18 21:58:33 +0800

    fix mavlink stuff & use a new thread for heartbeat

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ MSG_RATE_MAX = 30  # TODO in Hz
 MIN_DISTANCE = 0.35  # TODO in m
 MAX_DISTANCE = 35  # TODO in m
 
+
 import os
+
+os.environ["MAVLINK20"] = "1"  # Set MAVLink protocol to 2
+# os.environ["MAVLINK_DIALECT"] = "ardupilotmega"  # Default: ardupilotmega
+
+
+import threading
 import time
 
 import depthai as dai
@@ -20,10 +27,7 @@ from pymavlink import mavutil
 from pipeline import create_pipeline
 from z2xy import z2xy_coefficient, z2xy_coefficient_fov
 
-os.environ["MAVLINK20"] = "1"  # Set MAVLink protocol to 2
-# os.environ["MAVLINK_DIALECT"] = "ardupilotmega"  # Default: ardupilotmega
-
-
+exit = False
 connection = mavutil.mavlink_connection(
     device=DEVICE_STR,
     baud=BAUD_RATE,
@@ -37,11 +41,29 @@ get_current_time = lambda: int(round(time.time() * 1000) - start_time)  # in ms
 message_interval_min = 1000 / MSG_RATE_MAX  # in ms
 
 
-last_heartbeat_time = 0  # in ms
-last_message_time = 0  # in ms
+# Heartbeat thread
+def heartbeat():
+    global exit, connection
+    while not exit:
+        connection.mav.heartbeat_send(
+            mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
+            mavutil.mavlink.MAV_AUTOPILOT_INVALID,
+            0,
+            0,
+            0,
+        )
+        print(f"{get_current_time()} Heartbeat sent")
+        time.sleep(1)  # 1Hz
+
+
+heartbeat_thread = threading.Thread(target=heartbeat)
+heartbeat_thread.start()
+
+
+# Main
 with dai.Device() as device:
-    # Getting calibration data
     try:
+        # Getting calibration data
         calibData = device.readCalibration2()
 
         lensPosition = calibData.getLensPosition(dai.CameraBoardSocket.RGB)
@@ -54,69 +76,68 @@ with dai.Device() as device:
 
         hfov = calibData.getFov(dai.CameraBoardSocket.RGB)
         print(f"RGB Cam HFOV: {hfov}")
-    except:
-        raise Exception("Fail to get calibration data!")
 
-    # Generating fixed z to x,y coefficient
-    assert INPUT_SHAPE[1] % GRID_NUM_H == 0
-    assert INPUT_SHAPE[0] % GRID_NUM_W == 0
-    grid_height = INPUT_SHAPE[1] // GRID_NUM_H
-    grid_width = INPUT_SHAPE[0] // GRID_NUM_W
-    # Use intrinsic matrix
-    z2x, z2y = z2xy_coefficient(
-        grid_height, grid_width, GRID_NUM_H, GRID_NUM_W, np.array(intrinsics)
-    )
-    # Use HFOV only
-    """
-    z2x, z2y = z2xy_coefficient_fov(
-        grid_height, grid_width, GRID_NUM_H, GRID_NUM_W, hfov
-    )
-    """
-    z2x = z2x.reshape(GRID_NUM_H, GRID_NUM_W)
-    z2y = z2y.reshape(GRID_NUM_H, GRID_NUM_W)
+        # Generating fixed z to x,y coefficient
+        assert INPUT_SHAPE[1] % GRID_NUM_H == 0
+        assert INPUT_SHAPE[0] % GRID_NUM_W == 0
+        grid_height = INPUT_SHAPE[1] // GRID_NUM_H
+        grid_width = INPUT_SHAPE[0] // GRID_NUM_W
+        # Use intrinsic matrix
+        z2x, z2y = z2xy_coefficient(
+            grid_height, grid_width, GRID_NUM_H, GRID_NUM_W, np.array(intrinsics)
+        )
+        # Use HFOV only
+        """
+        z2x, z2y = z2xy_coefficient_fov(
+            grid_height, grid_width, GRID_NUM_H, GRID_NUM_W, hfov
+        )
+        """
+        z2x = z2x.reshape(GRID_NUM_H, GRID_NUM_W)
+        z2y = z2y.reshape(GRID_NUM_H, GRID_NUM_W)
 
-    device.startPipeline(create_pipeline(lensPosition=lensPosition))
-    q_nn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)  # type: ignore
+        # Start pipeline
+        device.startPipeline(create_pipeline(lensPosition=lensPosition))
+        q_nn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)  # type: ignore
 
-    while True:
-        current_time = get_current_time()
+        # Main loop
+        last_message_time = 0  # in ms
+        while True:
+            current_time = get_current_time()
 
-        # Try to get label and depth(z)
-        msgs = q_nn.get()
-        grids = msgs.getLayerFp16("out")
-        grids = np.asarray(grids).reshape(GRID_NUM_H, GRID_NUM_W, 2)  # label,z(in m)
+            # Try to get label and depth(z)
+            msgs = q_nn.get()
+            grids = msgs.getLayerFp16("out")
+            grids = np.asarray(grids).reshape(
+                GRID_NUM_H, GRID_NUM_W, 2
+            )  # label,z(in m)
 
-        # Sending heartbeat
-        if current_time - last_heartbeat_time >= 1000:  # 1Hz
-            connection.mav.heartbeat_send(
-                mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
-                mavutil.mavlink.MAV_AUTOPILOT_INVALID,
-                0,
-                0,
-                0,
-            )
-            last_heartbeat_time = current_time
-            print(f"{current_time} Heartbeat sent")
+            # Sending obstacle lacations
+            if current_time - last_message_time >= message_interval_min:
+                for i in range(GRID_NUM_H):
+                    for j in range(GRID_NUM_W):
+                        if grids[i][j][0] > 0:  # label>1
+                            z = grids[i][j][1]  # depth(z) in m
+                            x = z2x[i][j] * z  # in m
+                            y = z2y[i][j] * z  # in m
 
-        # Sending obstacle lacations
-        if current_time - last_message_time >= message_interval_min:
-            for i in range(GRID_NUM_H):
-                for j in range(GRID_NUM_W):
-                    if grids[i][j][0] > 0:  # label>1
-                        z = grids[i][j][1]  # depth(z) in m
-                        x = z2x[i][j] * z  # in m
-                        y = z2y[i][j] * z  # in m
+                            connection.mav.obstacle_distance_3d_send(
+                                current_time,  # UNIX Timestamp in ms
+                                4,  # MAV_DISTANCE_SENSOR_UNKNOWN
+                                12,  # MAV_FRAME_BODY_FRD
+                                65535,  # UINT16_MAX (Unknown)
+                                float(z),  # Forward, in m
+                                float(x),  # Right, in m
+                                float(-y),  # Down, in m
+                                float(MIN_DISTANCE),  # in m
+                                float(MAX_DISTANCE),  # in m
+                            )
+                            print(f"{current_time} x:{x:.2f}m y:{y:.2f}m z:{z:.2f}m")
+                last_message_time = current_time
 
-                        connection.mav.obstacle_distance_3d_send(
-                            current_time,  # UNIX Timestamp in ms
-                            4,  # MAV_DISTANCE_SENSOR_UNKNOWN
-                            0,  # MAV_FRAME_GLOBAL
-                            65535,  # UINT16_MAX (Unknown)
-                            float(x),  # in m
-                            float(y),  # in m
-                            float(z),  # in m
-                            float(MIN_DISTANCE),  # in m
-                            float(MAX_DISTANCE),  # in m
-                        )
-                        print(f"{current_time} x:{x:.2f}m y:{y:.2f}m z:{z:.2f}m")
-            last_message_time = current_time
+    except Exception as e:
+        print(e)
+
+    finally:
+        exit = True
+        heartbeat_thread.join()
+        connection.close()
